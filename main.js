9a6b4ec3dcd981d245fe3630312f04c8446785db
shubham <jagtapshubhamsham@gmail.com>
2024-10-08 04:15:35 +0530

    callback functions

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-function sum(a, b) {
-    return a + b;
+function sum(num1, num2, fnToCall) {
+    let result = num1 + num2;
+    fnToCall(result);
 }
 
-const ans = sum(2, 22);
-console.log(ans);
+function displayResult(data) {
+    console.log("Result of sum is : " + data);
+}
+
+function displayResultPassive(data) {
+    console.log("Sum's result is : " + data);
+}
+
+let ans = sum(1, 2, displayResult);
+// console.log(ans);
