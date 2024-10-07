f854641daa54ad94ad67e292cf86caa5bcbc84a6
shubham <jagtapshubhamsham@gmail.com>
2024-10-08 04:05:24 +0530

    basic understanding of functions

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-let personeName = ["Shubham", "Sakshi", "Aditya", "Om", "Vivek"];
-let personeGender = ["male", "female", "male", "male", "male"];
-let numberOfPersones = personeName.length;
-
-for (let i = 0; i < numberOfPersones; i++) {
-    if(personeGender[i] == "male") {
-        console.log(personeName[i] + " is male");
-    } else {
-        console.log(personeName[i] + " is female");
-    }
+function sum(a, b) {
+    return a + b;
 }
+
+const ans = sum(2, 22);
+console.log(ans);
