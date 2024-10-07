0dc43c538e2ad018d714a2d058d74f78ac6b6938
shubham <jagtapshubhamsham@gmail.com>
2024-10-08 03:02:54 +0530

    integer array, dine

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let age = [22, 34, 23, 45, 35, 43, 68];
 const numberOfPeople = age.length;
-
+let maxNumber = age[0];
 for(let i = 0; i <= numberOfPeople; i++) {
-    if(age[i] % 2 == 0) {
-        console.log(age[i]);
+    if(age[i] >= maxNumber ) {
+        maxNumber = age[i];
     }
 }
+console.log(maxNumber);
