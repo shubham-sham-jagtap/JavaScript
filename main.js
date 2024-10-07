d10a5add78e224e629bf0caf9d723ff5f38fcd79
shubham <jagtapshubhamsham@gmail.com>
2024-10-08 02:50:04 +0530

    Array

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-let answer = 0
+let age = [22, 34, 23, 45, 35, 43, 68];
+const numberOfPeople = age.length;
 
-for(let i = 0; i <= 1000; i++) {
-    answer  = answer + i;
+for(let i = 0; i <= numberOfPeople; i++) {
+    if(age[i] % 2 == 0) {
+        console.log(age[i]);
+    }
 }
-
-console.log(answer);
