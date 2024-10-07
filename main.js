e789c2112b388b623a844675d3055ec97be1b0ea
shubham <jagtapshubhamsham@gmail.com>
2024-10-08 02:32:09 +0530

    started with JS, done with understanding of statements

---
+++ main.js
@@ -0,0 +1,11 @@
+let firstName = "Shubham";
+let age = 20;
+let isMarried = false;
+
+// console.log("this persons name is " + firstName + " and his age is " + age);
+///////
+if(isMarried == true) {
+    console.log(firstName + " is Married");
+} else {
+    console.log(firstName + " is not Married");
+}
